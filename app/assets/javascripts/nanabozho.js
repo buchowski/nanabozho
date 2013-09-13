f2631ff77b9ec18f1ce271a11b3727cf316aae46
buchowski <bucholz.adam@gmail.com>
2013-09-13 11:12:46 -0700

    make user windows display side-by-side

--- app/assets/javascripts/nanabozho.js
+++ app/assets/javascripts/nanabozho.js
@@ -1,17 +1,21 @@
 function selectResult () {
 	console.log($(this).parent().attr('id'));
-	if ( $(this).parent().attr('id') == 'users' ) {
+	if ( $(this).parent().attr('id') == 'selected_users' ) {
+		$("#users_to_add").children("#user_" + $(this).children("input").attr('id')).remove();
 		$(this).remove();
-		$("#search_results").prepend($(this));
+		$("#searched_users").prepend($(this));
 	} else {
+		var user_id = $(this).children("input").attr('id');
+		$(this).children("input").clone().appendTo($("#users_to_add"));
+		$("#" + user_id).attr('id', 'user_' + user_id);
 		$(this).remove();
-		$("#users").prepend($(this));
+		$("#selected_users").prepend($(this));
 	}
 }
 function template (user) {
-	return "<div class='search_result'><img src='" + user['profile_image_url'] + "'>"
-			+ "<span>" + user['name'] + "</span><input class='user_id' type='hidden' " 
-			+ "name='group[users][]' id='" + user['id_str'] + "'></div>";
+	return "<div class='search_result'><img class='search_img' src='" + user['profile_image_url'] + "'>"
+			+ "<span class='username'>" + user['name'] + "</span><br><span class='location'>" + user['location'] + "</span>"
+			+ "<input class='user_id' type='hidden' name='group[users][]' id='" + user['id_str'] + "'></div>";
 }
 function header_template (user) {
 	return "<img src='" + user['profile_image_url'] + "'>"
@@ -25,8 +29,8 @@ function show_template (user) {
 	return "<div>" + header_template(user) + "</div><p id='" + user['twitter_id_str'] + "''></p>";
 }
 $(function () {
-	$("#search_results").on('click', '.search_result', selectResult)
-	$("#users").on('click', '.search_result', selectResult)
+	$("#selected_users").on('click', '.search_result', selectResult)
+	$("#searched_users").on('click', '.search_result', selectResult)
 	$("#search_form").on("submit", function (e) {
 		e.preventDefault();
 
@@ -37,9 +41,9 @@ $(function () {
 			dataType: 'json',
 			data: form.serialize(),
 			success: function (response) {
-				$("#users").empty();
+				$("#searched_users").empty();
 				for ( var i = 0; i < response.length; i++) {
-					$("#users").append(template(response[i]));
+					$("#searched_users").append(template(response[i]));
 					$("#" + response[i]['id_str']).val(JSON.stringify(response[i]));
 				}
 			},
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,3 +1,4 @@
+include UsersHelper
 class GroupsController < ApplicationController
   def index
   	@groups = Group.all
@@ -6,40 +7,8 @@ class GroupsController < ApplicationController
   def show
     @group = Group.find(params[:id])
     @users = @group.users
+    @tweets = tweets(@users)
 
-    consumer_key = OAuth::Consumer.new(
-        ENV['CONSUMER_KEY'],
-        ENV['CONSUMER_SECRET'])
-    access_token = OAuth::Token.new(
-        ENV['ACCESS_TOKEN'],
-        ENV['ACCESS_TOKEN_SECRET'])
-
-    @tweets = {}
-
-    @users.each do |user|
-      @tweets[user.id] = [] unless @tweets[user.id]
-
-      baseurl = "https://api.twitter.com"
-      path    = "/1.1/statuses/user_timeline.json"
-      query   = URI.encode_www_form("user_id" => user.twitter_id_str, "count" => 12) 
-      address = URI("#{baseurl}#{path}?#{query}") 
-
-      request = Net::HTTP::Get.new address.request_uri
-
-      http             = Net::HTTP.new address.host, address.port
-      http.use_ssl     = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-
-      request.oauth! http, consumer_key, access_token
-      http.start
-      response = http.request request
-      
-      if response.code == '200' 
-        temp = JSON.parse(response.body)
-        @tweets[user.id] << temp
-      end
-    end
-    #render :json => @tweets
     render :show
   end
 
@@ -124,8 +93,8 @@ class GroupsController < ApplicationController
       @membership.destroy
     end
     @users = @group.users
+    @tweets = tweets(@users)
     render :show
-    #render :text => "simply the best"
   end
   
 end
